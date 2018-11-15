4e2a6cb5e4d0a6b638771d218a55781583511457
Melchior522 <35888207+Melchior522@users.noreply.github.com>
2018-11-15 20:32:06 +0800

    Add files via upload

--- web-app/public/scripts/member.js
+++ web-app/public/scripts/member.js
@@ -110,7 +110,45 @@ function updateMember() {
   });
   return data.credit;
 }
-var credit=updateMember();
+function updateCredit(){
+  var formAccountNum = $('.account-number input').val();
+  var formCardId = $('.card-id input').val();
+
+  //create json data
+  var inputData = '{' + '"accountnumber" : "' + formAccountNum + '", ' + '"cardid" : "' + formCardId + '"}';
+  console.log(inputData)
+
+  //make ajax call
+  $.ajax({
+    type: 'POST',
+    url: apiUrl + 'memberData',
+    data: inputData,
+    dataType: 'json',
+    contentType: 'application/json',
+    beforeSend: function() {
+    },
+    success: function(data) {
+      //check data for error
+      if (data.error) {
+        alert(data.error);
+        return;
+      } else {
+        }
+      },
+    error: function(jqXHR, textStatus, errorThrown) {
+      //reload on error
+      alert("Error: Try again")
+      console.log(errorThrown);
+      console.log(textStatus);
+      console.log(jqXHR);
+    },
+    complete: function() {
+
+    }
+  });
+  return data.credit;
+}
+var credit=updatecredit();
 
 $('.earn-points-30').click(function() {
   earnPoints(30);
